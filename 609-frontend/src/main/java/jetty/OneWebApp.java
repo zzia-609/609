4f3604722f391e7b71b5b508eb60db142df29c5b
conner.qin <conner.qin@analyticservice.net>
2016-05-24 17:45:43 +0800

    jetty

---
+++ 609-frontend/src/main/java/jetty/OneWebApp.java
@@ -0,0 +1,51 @@
+package jetty;
+import java.io.File;
+import java.lang.management.ManagementFactory;
+
+import org.eclipse.jetty.jmx.MBeanContainer;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AllowSymLinkAliasChecker;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class OneWebApp {
+	
+	public static void main(String[] args) throws Exception {
+		// Create a basic jetty server object that will listen on port 8080.
+		// Note that if you set this to port 0 then a randomly available port
+		// will be assigned that you can either look in the logs for the port,
+		// or programmatically obtain it for use in test cases.
+		Server server = new Server(8080);
+
+		// Setup JMX
+		MBeanContainer mbContainer = new MBeanContainer(ManagementFactory.getPlatformMBeanServer());
+		server.addBean(mbContainer);
+
+		// The WebAppContext is the entity that controls the environment in
+		// which a web application lives and breathes. In this example the
+		// context path is being set to "/" so it is suitable for serving root
+		// context requests and then we see it setting the location of the war.
+		// A whole host of other configurations are available, ranging from
+		// configuring to support annotation scanning in the webapp (through
+		// PlusConfiguration) to choosing where the webapp will unpack itself.
+		WebAppContext webapp = new WebAppContext();
+		String basePath = System.getProperty("user.dir");
+		webapp.setContextPath("/609");
+		webapp.setDescriptor(basePath + "/src/main/webapp/WEB-INF/web.xml");
+		webapp.setResourceAlias("", "");
+//		webapp.addAliasCheck(new AllowSymLinkAliasChecker());
+
+		// A WebAppContext is a ContextHandler as well so it needs to be set to
+		// the server so it is aware of where to send the appropriate requests.
+		server.setHandler(webapp);
+		
+		// Start things up!
+		server.start();
+
+		// The use of server.join() the will make the current thread join and
+		// wait until the server is done executing.
+		// See
+		// http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
+		server.join();
+	}
+}
