24caf948cbc36ea87f6e61b73c072424952a9f23
puwu2013 <morenpuwukk@gmail.com>
2016-05-24 10:23:16 +0800

    merge bootstrap8; add springmvc

--- 609-web/src/main/java/com/zzia609/controller/BaseController.java
+++ 609-web/src/main/java/com/zzia609/controller/BaseController.java
@@ -1,7 +1,26 @@
 package com.zzia609.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
+@Controller
+@RequestMapping("/base")
 public class BaseController extends MultiActionController {
 	
+	@ResponseBody
+	@RequestMapping("/test.html")
+	public Object test (HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("key1", "abc");
+		map.put("key2", "def");
+		return map;
+	}
 }
